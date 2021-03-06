b1bb7c0558628281346a9674a762f782065ef24f
Julien Rabinow <jnr305@nyu.edu>
2021-03-05 17:19:04 -0800

    reset_mtimes handles edge cases; cleaner implementation

--- reset_mtimes.py
+++ reset_mtimes.py
@@ -33,14 +33,29 @@ def walkdir(dirname, match_regex=None, ignore_regex=None, invert=False):
             yield filepath, filename
 
 
+def assemble_extraction_regex(matching_file_prefix):
+    year = r"(\d{4})"
+    month = r"(\d{2})"
+    day = r"(\d{2})"
+    hour = r"(\d{2})"
+    minute = r"(\d{2})"
+    second = r"(\d{2})"
+    millisec_opt = r"(\d{3})?"
+    timestamp_opt = r"(_\d{13})?"
+    burst_suffix_opt = r"(_(BURST\d\d)?\d)?"
+    suffix_opt = r"(_(HDR|COVER(_TOP)?|TOP))?"
+    extract_date = f"{matching_file_prefix}{year}{month}{day}_{hour}{minute}{second}{millisec_opt}{timestamp_opt}{burst_suffix_opt}{suffix_opt}\."
+    extract_date_re = re.compile(extract_date)
+    return extract_date_re
+
+
 def main():
-    matching_file = r"(IMG|PANO|VID)_"
+    matching_file_prefix = r"(IMG|PANO|TINYPLANET_PANO|VID)_"
+    matching_file_prefix_re = re.compile(matching_file_prefix)
+    extract_date_re = assemble_extraction_regex(matching_file_prefix)
 
-    matching_file_re = re.compile(matching_file)
-    extract_date_re = re.compile(r"(IMG|PANO|VID)_(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})(\d{3})?(_(BURST\d\d)?\d)?(_HDR)?\.")
-
-    for filepath, filename in walkdir("/storage/emulated/0/DCIM/Camera", match_regex=matching_file_re, invert=False):
-        #print(filepath)
+    for filepath, filename in walkdir("/storage/emulated/0/DCIM/Camera", match_regex=matching_file_prefix_re, invert=False):
+        LOG.debug(filepath)
         match = extract_date_re.match(filename)
         if match:
             year = match.group(2)
@@ -49,15 +64,16 @@ def main():
             hour = match.group(5)
             minute = match.group(6)
             second = match.group(7)
-            #print("YEAR = {}\nMONTH = {}\nDAY = {}\nHOUR = {}\nMINUTE = {}\nSECOND = {}".format(
-            #    year, month, day, hour, minute, second
-            #))
+            LOG.debug("YEAR = {}\nMONTH = {}\nDAY = {}\nHOUR = {}\nMINUTE = {}\nSECOND = {}".format(
+                year, month, day, hour, minute, second
+            ))
             date_str = "{}-{}-{} {}:{}:{}".format(year, month, day, hour, minute, second)
             ret = os.system("touch -m -d '{}' {}".format(date_str, filepath))
             if ret != 0:
                 print("FAILED FOR FILE {}".format(filepath))
         else:
-            print("invalid file {}".format(filepath))
+            if not os.path.isdir(filepath):
+                print("invalid file {}".format(filepath))
 
 
 if __name__ == "__main__":
